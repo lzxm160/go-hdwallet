262a78c19c4006c5a43e142e42bc74f5a9256679
limingming <limingming@wident.cn>
2017-11-23 12:20:04 +0800

    x

--- gowallet.go
+++ gowallet.go
@@ -1,7 +1,7 @@
 package main
 /* 
 #cgo  CFLAGS:  -I  /root/bip44cxx 
-#cgo  LDFLAGS:  -L /root/bip44cxx  -L /root/bitcoin-0.15.1/src -lbip44wallet -lbitcoin -lbitcoin-client -lbitcoin_server -lbitcoin_util -lbitcoin_consensus -lbitcoin_common -lbitcoin_cli -lbitcoin_crypto
+#cgo  LDFLAGS:  -L /root/bip44cxx  -L /root/bitcoin-0.15.1/src -lbip44wallet -lbitcoin -lbitcoin-client -lbitcoin_server -lbitcoin_util -lbitcoin_consensus -lbitcoin_common -lbitcoin_cli
 #include <stdlib.h>
 #include "interface.h" 
 #include "interface2.h"
