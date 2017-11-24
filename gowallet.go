610050eefa0806c7d7658b969186708edc2902f2
limingming <limingming@wident.cn>
2017-11-24 13:23:15 +0800

    x

--- gowallet.go
+++ gowallet.go
@@ -1,7 +1,7 @@
 package main
 /* 
 #cgo  CFLAGS:  -I  /root/bip44cxx 
-#cgo  LDFLAGS:  -L /root/bip44cxx -lbip44wallet
+#cgo  LDFLAGS:  -L /root/bip44cxx -lbip44wallet -lbitcoin -lbitcoin-client
 #include <stdlib.h>
 #include "interface.h"
 */  
@@ -196,23 +196,23 @@ func test2() {
 	fmt.Println("-------------------------------------")
 
 {
-	//use btcd
+	
+	///CreateRawTransaction用btcd的，只能在btcd中运行，属于main包无法导出
+	///sign用btcwallet中的，因为btcd中没有wallet的实现
 	// txInputs := []btcjson.TransactionInput{
-	// 				{Txid: "6c3f611cbd624e8a094f08b10f849b765d3548c13ace1704de050a44f504caff", Vout: 0},
-	// 			}
-	// 			amounts := map[string]float64{"mxu9tvJsuZq1rxiaevcUJkuu6mv2LFhpSr": 0.1}
-	// cmd:=btcjson.NewCreateRawTransactionCmd(txInputs, amounts, nil)
+	// 	{Txid: "6c3f611cbd624e8a094f08b10f849b765d3548c13ace1704de050a44f504caff", Vout: 0},
+	// }
+	// amounts := map[string]float64{"mxu9tvJsuZq1rxiaevcUJkuu6mv2LFhpSr": 0.1}
+	// cmd := btcjson.NewCreateRawTransactionCmd(txInputs, amounts, nil)
 	// // func HandleCreateRawTransaction(s *rpcServer, cmd interface{}, closeChan <-chan struct{}) (interface{}, error)
-	// closeChan:=make(chan,1)
-	// ret,err:=HandleCreateRawTransaction(nil,cmd,closeChan)
-	// if err!=nil{
+	// // closeChan := make(chan struct{})
+	// ret, err := HandleCreateRawTransaction(cmd)
+	// if err != nil {
 	// 	fmt.Println(err)
 	// }
 	// fmt.Println(ret)
-
-	// func SignRawTransaction(icmd interface{}, w *wallet.Wallet, chainClient *chain.RPCClient) (interface{}, error)
-	// `[{"txid":"6c3f611cbd624e8a094f08b10f849b765d3548c13ace1704de050a44f504caff","vout":n,"scriptpubkey":"value","redeemscript":"value"}] ["privkey"] flags="ALL"`
-	// cmd:=`"signrawtransaction 0100000001ffca04f5440a05de0417ce3ac148355d769b840fb1084f098a4e62bd1c613f6c0000000000ffffffff0180969800000000001976a914beacf93f739b48324e79d5c3314c8a434d18d2ba88ac00000000 ["privkey"]"`
+	// // <-closeChan
+	// time.Sleep(10000 * time.Millisecond)
 	txInputs := []btcjson.RawTxInput{} 				
 	privKeys := []string{"cVJiFesQn1duqM6RThR3N8oXL6xkYFo1r5h4PtCaXV3qXkxd3DBT"} 				
 	cmd:=btcjson.NewSignRawTransactionCmd("0100000001ffca04f5440a05de0417ce3ac148355d769b840fb1084f098a4e62bd1c613f6c0000000000ffffffff0180969800000000001976a914beacf93f739b48324e79d5c3314c8a434d18d2ba88ac00000000", &txInputs, &privKeys, nil)
