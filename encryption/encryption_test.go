f5363cd13882eada21c21004b376726a819141e6
limingming <limingming@wident.cn>
2017-11-27 14:21:46 +0800

    x

--- encryption/encryption_test.go
+++ encryption/encryption_test.go
@@ -13,7 +13,7 @@ func Test1(t *testing.T) {
 		{
 			Key:     "123456",
 			MasterKey:       "xprv9s21ZrQH143K3d9R9oDAj9j1PkbWuUkqi4TT2RgWqTTvgmahbNW9cxccRhEWSfFPHhKar6nqGYxukp5BjrvFqCjoLTxQ9izmBmes4sSR7KH",
-		}
+		},
 	}
 
 	for _, v := range ts_data {
