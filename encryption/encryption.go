5393a4e3aa83bed4c5361244fb4765087b33c616
limingming <limingming@wident.cn>
2017-11-27 19:36:42 +0800

    x

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -40,14 +40,13 @@ func encrypt(key, text []byte) []byte {
 	cfb.XORKeyStream(ciphertext[aes.BlockSize:], b)
 	return ciphertext
 }
-func decrypt(key, text []byte) []byte {
+func decrypt(key, text []byte) ([]byte,error){
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return []byte(""),err
 	}
 	if len(text) < aes.BlockSize {
-		fmt.Printf("Error!\n")
-		os.Exit(0)
+		return []byte(""),errors.New("len(text) < aes.BlockSize")
 	}
 	iv := text[:aes.BlockSize]
 	text = text[aes.BlockSize:]
@@ -57,15 +56,18 @@ func decrypt(key, text []byte) []byte {
 }
 func byteSliceEqual(a, b []byte) bool {
     if len(a) != len(b) {
+    	fmt.Println("len")
         return false
     }
 
     if (a == nil) != (b == nil) {
+    	fmt.Println("nil")
         return false
     }
 
     for i, v := range a {
         if v != b[i] {
+        	fmt.Println("range")
             return false
         }
     }
@@ -97,7 +99,10 @@ func Encrypt(key, text []byte) []byte {
 //用密码对密文解密返回masterkey对应的byte数组
 func Decrypt(key, text []byte) []byte {
 	hashKey:=sha256.Sum256(key)
-	d_des:=decrypt(hashKey[:], text[:len(text)-len(hashKey)])
+	d_des,err:=decrypt(hashKey[:], text[:len(text)-len(hashKey)])
+	if err!=nil{
+		return []byte("")
+	}
 	return d_des[:len(d_des)-len(hashKey)]
 }
 //用文本来验证密码是否正确
@@ -109,7 +114,10 @@ func Validate(key, text []byte) bool {
 
 	// fmt.Println("suffix:",suffix)
 
-	d_des:=decrypt(hashKey[:], text[:len(text)-len(hashKey)])
+	d_des,err:=decrypt(hashKey[:], text[:len(text)-len(hashKey)])
+	if err!=nil{
+		return false
+	}
 	hash_des:=sha256.Sum256(d_des[:])
 	// fmt.Println("d_des:",d_des)
 	// fmt.Println("d_des:",hex.EncodeToString(d_des[:len(d_des)-len(suffix)]))
